096699df6edb7322334550c7c9a8623c1325fd05
chasingstars <maydayfantacy@gmail.com>
2021-06-07 20:46:35 +0800

    jjj

--- app/models_for_line.py
+++ app/models_for_line.py
@@ -12,10 +12,29 @@ def handle_message(event):
         TextSendMessage(text=f"Hello {line_bot_api.get_profile(event.source.user_id).display_name}!")
     )
 
-"""   
+
 # 學你說話
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
     line_bot_api.reply_message(
         event.reply_token, TextSendMessage(text=event.message.text)
     )
+"""       
+# Phoebe愛唱歌
+@handler.add(MessageEvent, message=TextMessage)
+def pretty_echo(event):
+    
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        
+        pretty_note = '♫♪♬'
+        pretty_text = ''
+        
+        for i in event.message.text:
+        
+            pretty_text += i
+            pretty_text += random.choice(pretty_note)
+    
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=pretty_text)
+        )
