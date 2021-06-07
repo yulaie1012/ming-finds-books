220494d3dbf01c7a09b028efa7b442eb27782157
chasingstars <maydayfantacy@gmail.com>
2021-06-07 19:29:57 +0800

    這次更新的註解

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,12 @@ import os
 
 from flask import Flask
 from linebot import LineBotApi, WebhookHandler
-
+import configparser
 app = Flask(__name__)
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
