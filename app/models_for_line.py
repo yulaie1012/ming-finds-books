e7029d3d869b8ce9b69a0e057c7fd3ba33c67e24
chasingstars <maydayfantacy@gmail.com>
2021-06-07 20:17:32 +0800

    fuck

--- app/models_for_line.py
+++ app/models_for_line.py
@@ -17,6 +17,5 @@ def handle_message(event):
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
     line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
+        event.reply_token, TextSendMessage(text=event.message.text)
     )
