9a95d4d97d3193f1430d108649e7db078d775c0a
chasingstars <maydayfantacy@gmail.com>
2021-06-07 20:34:20 +0800

    這次更新的註解

--- app/models_for_line.py
+++ app/models_for_line.py
@@ -4,18 +4,19 @@ from app import handler, line_bot_api
 
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-"""
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token, 
         TextSendMessage(text=f"Hello {line_bot_api.get_profile(event.source.user_id).display_name}!")
     )
-"""
-    
+
+"""   
 # 學你說話
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
     line_bot_api.reply_message(
         event.reply_token, TextSendMessage(text=event.message.text)
     )
+"""
