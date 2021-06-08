c917f8dc9e8447d416e1fb28286a637e22f20371
chasingstars <maydayfantacy@gmail.com>
2021-06-08 14:51:14 +0800

    l

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,6 @@
 import os
 from flask import Flask
 from linebot import LineBotApi, WebhookHandler
-import configparser
-from app import handler, line_bot_api
-
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
@@ -14,12 +11,6 @@ chrome_options.add_argument("--no-sandbox")
 from selenium import webdriver # 先下載 webdriver
 chrome_options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(executable_path=os.environ.get("C:\\Users\mayda\Downloads\chromedriver"), chrome_options=chrome_options)
-
-
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-import time
 
 
 app = Flask(__name__)
