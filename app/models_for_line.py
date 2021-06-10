02594b0f12243313f4a9d9473a7f4ec8bbedcf4a
chasingstars <maydayfantacy@gmail.com>
2021-06-10 15:53:57 +0800

    d

--- app/models_for_line.py
+++ app/models_for_line.py
@@ -3,22 +3,22 @@
 from app import handler, line_bot_api
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-def findbooks(owo):
-    
-    from selenium import webdriver
-    import os
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+from selenium import webdriver
+import os
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-    from selenium.webdriver.support.ui import Select
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.common.action_chains import ActionChains
-    import time
-    
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+def findbooks(owo):
+       
     driver.get("https://metacat.ntu.edu.tw") # 更改網址以前往不同網頁
 
     ISBN = owo
