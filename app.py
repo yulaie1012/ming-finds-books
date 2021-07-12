e5b5c5459750143db4370e1d4dba81b229c91384
ouyang-yang <maydayfantacy@gmail.com>
2021-07-12 10:44:53 +0800

    0712-10:44

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def callback():
 
 #----------------設定回覆訊息介面-----------------
 @handler.add(MessageEvent, message=TextMessage)
-def NTNU_crawling(keyevent):
+def NTNU_crawling(event):
     #----------------取得userid-----------------
     user_id = event.source.user_id
     if user_id == '':
@@ -89,29 +89,30 @@ def NTNU_crawling(keyevent):
 
 
     #----------------爬蟲-----------------    
-    ISBN = keyevent.message.text
-    urltest = "https://libholding.ntut.edu.tw/webpacIndex.jsp"
-    driver = webdriver.Chrome("C:\\Users\mayda\Downloads\chromedriver") 
-    driver.get(urltest)
-    element = driver.find_element_by_id('search_inputS')
-    element.send_keys(ISBN)
-    select = Select(driver.find_element_by_id('search_field'))
-    select.select_by_value("STANDARDNO")
-    search_gogogo = driver.find_element_by_xpath('/html/body/div[2]/table/tbody/tr/td[1]/div/div/div[1]/div/div[1]/div/form/table/tbody/tr[2]/td/input[3]').click()
+    if event.isalnum(): #所有字元都是數字或者字母
+        ISBN = event.message.text
+        urltest = "https://libholding.ntut.edu.tw/webpacIndex.jsp"
+        driver = webdriver.Chrome("C:\\Users\mayda\Downloads\chromedriver") 
+        driver.get(urltest)
+        element = driver.find_element_by_id('search_inputS')
+        element.send_keys(ISBN)
+        select = Select(driver.find_element_by_id('search_field'))
+        select.select_by_value("STANDARDNO")
+        search_gogogo = driver.find_element_by_xpath('/html/body/div[2]/table/tbody/tr/td[1]/div/div/div[1]/div/div[1]/div/form/table/tbody/tr[2]/td/input[3]').click()
 
-    time.sleep(5)
-    output = str()
-    table = driver.find_element_by_class_name('order')
-    trlist = table.find_elements_by_tag_name('tr')
-    for row in trlist:
-        tdlist = row.find_elements_by_tag_name('td')
-        for sth in tdlist:
-            output = tdlist[2].text +" "+ tdlist[8].text
-            break
+        time.sleep(5)
+        output = str()
+        table = driver.find_element_by_class_name('order')
+        trlist = table.find_elements_by_tag_name('tr')
+        for row in trlist:
+            tdlist = row.find_elements_by_tag_name('td')
+            for sth in tdlist:
+                output = tdlist[2].text +" "+ tdlist[8].text
+                break
     
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
         line_bot_api.reply_message(
-            keyevent.reply_token,
+            event.reply_token,
             TextSendMessage(text=output)
         )
 
